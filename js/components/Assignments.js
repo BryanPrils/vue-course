47025656837268696afe603bdbda030066bad1f8
Bryan Prils <bryan@bmksolutions.be>
2023-01-18 15:24:37 +0100

    episode 8

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -5,8 +5,14 @@ export default {
   template: `
     <section class="space-y-6">
       <assignment-list :assignments="filters.inProgress" title="In progress"></assignment-list>
-
       <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+    
+      <form @submit.prevent="add">
+        <div class="border border-gray-600 text-black">
+          <input v-model="newAssignment" placeholder="New assignment..." class="p-2">
+          <button type="submit" class="bg-white text-black p-2 border-l">Add</button>
+        </div>
+      </form>
     </section>
   `,
   data () {
@@ -15,7 +21,8 @@ export default {
         { name: 'Finish project', complete: false, id: 1 },
         { name: 'Read chapter 4', complete: false, id: 2 },
         { name: 'Turn in homework', complete: false, id: 3 },
-      ]
+      ],
+      newAssignment: ''
     }
   },
 
@@ -25,6 +32,18 @@ export default {
         inProgress: this.assignments.filter(assignment => !assignment.complete),
         completed: this.assignments.filter(assignment => assignment.complete),
       }
+    }
+  },
+
+  methods:{
+    add(){
+      this.assignments.push({
+        name: this.newAssignment,
+        complete: false,
+        id: this.assignments.length +1
+      });
+
+      this.newAssignment = '';
     }
   }
 }
