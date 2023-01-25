81b70317e0e92a9c0d8969c06ae08e49f88bae18
Bryan Prils <bryan@bmksolutions.be>
2023-01-25 15:42:28 +0100

    episode 15

--- js/components/App.js
+++ js/components/App.js
@@ -1,8 +1,12 @@
 import Assignments from './Assignments.js'
+import Panel from './Panel.js'
 
 export default {
-  components: { Assignments },
+  components: { Panel, Assignments },
   template: `
+    <div class="grid gap-6">
     <assignments></assignments>
+
+    </div>
   `
 }
--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,33 +1,34 @@
 import Assignment from './Assignment.js'
 import AssignmentTags from './AssignmentTags.js'
+import Panel from './Panel.js'
 
 export default {
-  components: { Assignment, AssignmentTags },
+  components: { Assignment, AssignmentTags, Panel },
 
   template: `
-    <section v-show="assignments.length" class="w-60">
-    <div class="flex justify-between items-start">
-      <h2 class="font-bold mb-2">{{ title }}
-        <span>({{ assignments.length }})</span>
-      </h2>
-      <button v-show="canToggle" @click="$emit('toggle')">&times;</button>
-    </div>
+    <Panel v-show="assignments.length" class="w-60">
+      <div class="flex justify-between items-start">
+        <h2 class="font-bold mb-2">{{ title }}
+          <span>({{ assignments.length }})</span>
+        </h2>
+        <button v-show="canToggle" @click="$emit('toggle')">&times;</button>
+      </div>
+  
+      <assignment-tags
+          v-model:currentTag="currentTag"
+          :initial-tags="assignments.map(a=> a.tag)"
+      />
+  
+      <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
+        <assignment v-for="assignment in filteredAssignments"
+                    :key="assignment.id"
+                    :assignment="assignment"
+        ></assignment>
+      </ul>
+  
+      <slot></slot>
 
-    <assignment-tags
-        v-model:currentTag="currentTag"
-        :initial-tags="assignments.map(a=> a.tag)"
-    />
-
-    <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
-      <assignment v-for="assignment in filteredAssignments"
-                  :key="assignment.id"
-                  :assignment="assignment"
-      ></assignment>
-    </ul>
-
-    <slot></slot>
-
-    </section>
+    </Panel>
   `,
   props: {
     assignments: Array,
